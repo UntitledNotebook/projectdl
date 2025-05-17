7914898a899c44bbbf1e46ec4cbeedff77de2f5a
zcm24 <zcm24@mails.tsinghua.edu.cn>
2025-05-17 22:42:39 +0800

    Modified data selection

--- data.py
+++ data.py
@@ -5,30 +5,37 @@ from pathlib import Path
 import amulet
 from collections import defaultdict
 
-combined_counts = {}
+USE_PRE = False
 
-for subfolder in Path("data").iterdir():
-    if subfolder.is_dir():
-        counts_file = subfolder / "snbt_counts.json"
-        if counts_file.exists():
-            with open(counts_file, 'r') as f:
-                counts_data = json.load(f)
-                for key, value in counts_data.items():
-                    combined_counts[key] = combined_counts.get(key, 0) + value
+if USE_PRE:
+    id_to_snbt = json.load(open("data/id_to_snbt.json", 'r'))
+    id_to_snbt = {int(k): v for k, v in id_to_snbt.items()}
+    snbt_to_id = {v: int(k) for k, v in id_to_snbt.items()}
+else:
+    combined_counts = {}
 
-with open(Path("data") / "snbt_counts.json", 'w') as f:
-    json.dump(combined_counts, f, indent=2, sort_keys=True)
+    for subfolder in Path("data").iterdir():
+        if subfolder.is_dir():
+            counts_file = subfolder / "snbt_counts.json"
+            if counts_file.exists():
+                with open(counts_file, 'r') as f:
+                    counts_data = json.load(f)
+                    for key, value in counts_data.items():
+                        combined_counts[key] = combined_counts.get(key, 0) + value
 
-THRESHOLD = int(input("Enter the threshold for combined counts: "))
+    with open(Path("data") / "snbt_counts.json", 'w') as f:
+        json.dump(combined_counts, f, indent=2, sort_keys=True)
 
-combined_counts = {k: v for k, v in combined_counts.items() if v > THRESHOLD}
-air_snbt_str = amulet.Block("universal_minecraft", "air").snbt_blockstate
-combined_palette = set(combined_counts.keys())
-combined_palette.discard(air_snbt_str)
-sorted_other_snbt = sorted(list(combined_palette))
-snbt_to_id = {air_snbt_str: 0}
-snbt_to_id.update({snbt: i + 1 for i, snbt in enumerate(sorted_other_snbt)})
-id_to_snbt = {i: snbt for snbt, i in snbt_to_id.items()}
+    THRESHOLD = int(input("Enter the threshold for combined counts: "))
+
+    combined_counts = {k: v for k, v in combined_counts.items() if v > THRESHOLD}
+    air_snbt_str = amulet.Block("universal_minecraft", "air").snbt_blockstate
+    combined_palette = set(combined_counts.keys())
+    combined_palette.discard(air_snbt_str)
+    sorted_other_snbt = sorted(list(combined_palette))
+    snbt_to_id = {air_snbt_str: 0}
+    snbt_to_id.update({snbt: i + 1 for i, snbt in enumerate(sorted_other_snbt)})
+    id_to_snbt = {i: snbt for snbt, i in snbt_to_id.items()}
 
 dataset = defaultdict(list)
 
@@ -51,22 +58,29 @@ def process_subfolder(subfolder_path):
         height = metadata.get("region_height", 0)
     
     block_id_array = np.load(npy_file)
-    id_remap = {int(i): snbt_to_id.get(snbt, 0) for i, snbt in subfolder_id_to_snbt.items()}
-    remapped_array = np.vectorize(lambda x: id_remap.get(x, 0), otypes=[np.int32])(block_id_array)
-    
-    return (length, height), remapped_array
+    legal_local_ids = set([int(i) for i in subfolder_id_to_snbt.keys()\
+                           if subfolder_id_to_snbt[i] in snbt_to_id.keys()])
+    legal_id_remap = {i: snbt_to_id[subfolder_id_to_snbt[str(i)]] for i in legal_local_ids}
+    remapped_array = []
+    for i in range(block_id_array.shape[0]):
+        if np.all(np.isin(block_id_array[i], list(legal_local_ids))):
+            remapped_array.append(np.vectorize(lambda x: legal_id_remap.get(x, 0), otypes=[np.int32])(block_id_array[i]))
+    remapped_array = np.stack(remapped_array, axis=0)
+    counts = np.bincount(remapped_array.flatten(), minlength=len(snbt_to_id))
+    return (length, height), remapped_array, counts
 
 if __name__ == '__main__':
     subfolders = [subfolder for subfolder in Path("data").iterdir() if subfolder.is_dir()]
 
     with multiprocessing.Pool(processes=6) as pool:
         results = pool.map(process_subfolder, subfolders)
-
+    combined_counts = np.zeros(len(snbt_to_id), dtype=np.int32)
     for result in results:
         if result:
-            (length, height), remapped_array = result
+            (length, height), remapped_array, counts = result
             dataset[(length, height)].append(remapped_array)
-
+            combined_counts += counts
+    combined_counts = {id_to_snbt[i]: int(count) for i, count in enumerate(combined_counts)}
     with open(Path("data") / "id_to_snbt.json", 'w') as f:
         json.dump(id_to_snbt, f, indent=2, sort_keys=True)
     with open(Path("data") / "snbt_counts.json", 'w') as f:
